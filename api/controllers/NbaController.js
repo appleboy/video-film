0afb57d4188f39ab4ea8950fd1824c8b2fb3bfeb
Bo-Yi Wu <appleboy.tw@gmail.com>
2015-01-12 18:11:06 +0800

    update css.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- api/controllers/NbaController.js
+++ api/controllers/NbaController.js
@@ -14,14 +14,15 @@ module.exports = {
 
   show: function (req, res) {
     var tag = req.param('tag') || '';
-      ajax = _.contains(req.path, 'ajax');
+      ajax = _.contains(req.path, 'ajax'),
+      top_plays = _.contains(req.path, 'top_plays');
 
     if (tag === '') {
       return res.redirect('/');
     }
 
     Tag.videos(req, function (data) {
-      data = _.merge({tag: tag}, data);
+      data = _.merge({tag: tag, top_plays: top_plays}, data);
 
       return (ajax) ? res.view('nba/list', _.merge(data, {layout: null})) : res.view('nba/list', data);
     });
