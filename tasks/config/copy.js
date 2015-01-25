3a2e539a0de22461d5810b225412c4736c10e892
Bo-Yi Wu <appleboy.tw@gmail.com>
2015-01-26 01:45:13 +0800

    copy bootstrap fonts.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- tasks/config/copy.js
+++ tasks/config/copy.js
@@ -11,28 +11,43 @@
  * Copies all directories nd files from the .tmp/public directory into a www directory.
  *
  * For usage docs see:
- * 		https://github.com/gruntjs/grunt-contrib-copy
+ *    https://github.com/gruntjs/grunt-contrib-copy
  */
 module.exports = function(grunt) {
 
-	grunt.config.set('copy', {
-		dev: {
-			files: [{
-				expand: true,
-				cwd: './assets',
-				src: ['**/*.!(coffee|less)'],
-				dest: '.tmp/public'
-			}]
-		},
-		build: {
-			files: [{
-				expand: true,
-				cwd: '.tmp/public',
-				src: ['**/*'],
-				dest: 'www'
-			}]
-		}
-	});
+  grunt.config.set('copy', {
+    dev: {
+      files: [{
+        expand: true,
+        cwd: './assets',
+        src: ['**/*.!(coffee|less)'],
+        dest: '.tmp/public'
+      }, {
+        expand: true,
+        cwd: './assets/vendor/bootstrap',
+        src: ['fonts/**/*'],
+        dest: '.tmp/public'
+      }, {
+        expand: true,
+        cwd: './assets/vendor/font-awesome',
+        src: ['fonts/**/*'],
+        dest: '.tmp/public'
+      }, {
+        expand: true,
+        cwd: './assets/vendor/video.js/dist/video-js',
+        src: ['font/**/*'],
+        dest: '.tmp/public/min'
+      }]
+    },
+    build: {
+      files: [{
+        expand: true,
+        cwd: '.tmp/public',
+        src: ['**/*'],
+        dest: 'www'
+      }]
+    }
+  });
 
-	grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 };
