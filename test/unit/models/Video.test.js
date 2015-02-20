a1636e184858ec20f0485583e5042902286f40ea
Bo-Yi Wu <appleboy.tw@gmail.com>
2015-02-20 16:35:31 +0800

    add src extension testing.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- test/unit/models/Video.test.js
+++ test/unit/models/Video.test.js
@@ -10,7 +10,6 @@ describe('Videos', function() {
   it ('test increae on base model', function(done) {
     Video.increase('view_counts', 3);
     Video.increase('view_counts', 1, function(res) {
-      //console.log(res);
       Video.findOne(1)
         .exec(function(err, video) {
           video.view_counts.should.be.eql(11);
@@ -22,7 +21,6 @@ describe('Videos', function() {
   it ('test decreae on base model', function(done) {
     Video.decrease('view_counts', 4);
     Video.decrease('view_counts', 2, function(res) {
-      //console.log(res);
       Video.findOne(2)
         .exec(function(err, video) {
           video.view_counts.should.be.eql(19);
@@ -30,4 +28,12 @@ describe('Videos', function() {
       });
     });
   });
+
+  it ('get video extension name', function(done) {
+    Video.findOne(1).exec(function(err, video) {
+      video.getSrcExtName().should.eql('flv');
+      done();
+    });
+  });
+
 });
