08a9ef787fbeb8dd582c5c4e849caabc4faeb0c5
Bo-Yi Wu <appleboy.tw@gmail.com>
2015-02-22 17:06:15 +0800

    check variable exist.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- test/unit/models/User.test.js
+++ test/unit/models/User.test.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var should = require("should");
+
 describe('Users', function() {
   it('should not be empty', function(done) {
     User.find().exec(function(err, rows) {
@@ -10,6 +12,7 @@ describe('Users', function() {
 
   it('create user without password', function(done) {
     User.create({email:'test1@gmail.com', password: ''}).exec(function createCB(err, user) {
+      should.not.exists(err);
       user.should.be.an.instanceOf(Object).and.have.property('id').which.is.a.Number.and.be.above(0);
       done();
     });
@@ -17,6 +20,7 @@ describe('Users', function() {
 
   it('create user with password', function(done) {
     User.create({email:'test2@gmail.com', password: '123456'}).exec(function createCB(err, user) {
+      should.not.exists(err);
       user.should.be.an.instanceOf(Object).and.have.property('id').which.is.a.Number.and.be.above(0);
       user.password.length.should.be.above(10);
       done();
