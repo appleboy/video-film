45466fe2a7dd5b6626a49fb84993bd4fa3b66a96
Bo-Yi Wu <appleboy.tw@gmail.com>
2015-02-22 16:34:29 +0800

    update user model test.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- test/unit/models/User.test.js
+++ test/unit/models/User.test.js
@@ -3,22 +3,22 @@
 describe('Users', function() {
   it('should not be empty', function(done) {
     User.find().exec(function(err, rows) {
-      rows.length.should.be.eql(fixtures.user.length);
-
+      rows.length.should.be.eql(fixtures.user.length).and.be.above(0);
       done();
     });
   });
 
   it('create user without password', function(done) {
-    User.create({email:'test1@gmail.com', password: ''}).exec(function createCB(err, created){
-      console.log('Created user with email '+ created.email);
+    User.create({email:'test1@gmail.com', password: ''}).exec(function createCB(err, user) {
+      user.should.be.an.instanceOf(Object).and.have.property('id').which.is.a.Number.and.be.above(0);
       done();
     });
   });
 
   it('create user with password', function(done) {
-    User.create({email:'test2@gmail.com', password: '123456'}).exec(function createCB(err, created){
-      console.log('Created user with email '+ created.email);
+    User.create({email:'test2@gmail.com', password: '123456'}).exec(function createCB(err, user) {
+      user.should.be.an.instanceOf(Object).and.have.property('id').which.is.a.Number.and.be.above(0);
+      user.password.length.should.be.above(10);
       done();
     });
   });
